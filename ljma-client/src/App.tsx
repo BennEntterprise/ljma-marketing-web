ff40ab744ffe7b3b02b0336eef732b4c70882c7a
BennEntterprise <kyle@bennentterprise.com>
2020-12-15 22:23:11 +0000

    add bootstrap demo template to prove connections

--- ljma-client/src/App.tsx
+++ ljma-client/src/App.tsx
@@ -3,24 +3,28 @@ import logo from './logo.svg'
 import './App.css'
 import MyComponent from './MyComp'
 import Awesome from './Awesome'
+import Navbar from './components/Navbar'
 const App = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Laura Jean Music Academy</p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-        <MyComponent />
-        <Awesome />
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>Laura Jean Music Academy</p>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
+          <MyComponent />
+          <Awesome />
+        </header>
+      </div>
+    </>
   )
 }
 
