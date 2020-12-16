58a8a62f0ee33b0ddb948d8fe3d89ae1b832e63f
BennEntterprise <kyle@bennentterprise.com>
2020-12-16 18:37:56 +0000

    initial banner submission, sans bottomnav

--- ljma-client/src/pagelets/1Welcome/Welcome.tsx
+++ ljma-client/src/pagelets/1Welcome/Welcome.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
 import './Welcome.scss'
+import BannerImg from './BannerImg.jpg'
 const Welcome = () => {
-  return <div id='welcome-pagelet'>Welcome</div>
+  return (
+    <div id='welcome-pagelet'>
+      <div className='banner-text'>
+        <h5 className='liberation-sans-bold'>Welcome to</h5>
+        <h3 className='liberation-sans-bold'>Laura Jean Music Academy</h3>
+        <h6 className='liberation-sans-bold'>
+          <em>Developing young characters one note at at time.</em>
+        </h6>
+      </div>
+    </div>
+  )
 }
 
 export default Welcome
