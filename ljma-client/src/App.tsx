a7d042c7099292ed87e222afe9070d7277b4333f
BennEntterprise <kyle@bennentterprise.com>
2020-12-16 16:31:27 +0000

    add contact form simulated

--- ljma-client/src/App.tsx
+++ ljma-client/src/App.tsx
@@ -11,6 +11,7 @@ import Typefaces from './constants/Typefaces'
 // Import Main App Components and Pagelets
 // ********************
 import Navbar from './components/Navbar'
+import IndependentContactPagelet from './components/IndependentContactPagelet'
 import Welcome from './pagelets/1Welcome/Welcome'
 import AboutUs from './pagelets/2AboutUs/AboutUs'
 import Courses from './pagelets/3Courses/Courses'
@@ -18,7 +19,6 @@ import ArtistDevelopmentProgram from './pagelets/4ArtistDevelopmentProgram/Artis
 import Exams from './pagelets/5Exams/Exams'
 import MeetTheTeachers from './pagelets/6MeetTheTeachers/MeetTheTeachers'
 import BookLessons from './pagelets/7BookLessons/BookLessons'
-
 const App = () => {
   console.info('Is Dev Mode:', isDev())
   return (
@@ -37,6 +37,9 @@ const App = () => {
           <AuditHoop variant='complete'></AuditHoop>
           <AuditHoop variant='pending-approval'></AuditHoop>
           <AuditHoop variant='wip'></AuditHoop>
+        </Route>
+        <Route exact path='/contact'>
+          <IndependentContactPagelet />
         </Route>
       </Router>
     </>
