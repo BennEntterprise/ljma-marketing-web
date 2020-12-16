cb81c0eee190dd17946951417e63c31ebe138c91
BennEntterprise <kyle@bennentterprise.com>
2020-12-16 15:25:55 +0000

    change hoop styling, add flag for console to describe node env

--- ljma-client/src/utilities/isDev.tsx
+++ ljma-client/src/utilities/isDev.tsx
@@ -1,5 +1,6 @@
 import process from 'process'
 
+console.log(`The infrastructure mode is: ${process.env.NODE_ENV}`)
 const development: boolean =
   !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
 
